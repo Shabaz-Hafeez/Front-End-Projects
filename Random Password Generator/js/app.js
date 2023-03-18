21b7820a212cf557c88d1dbdb9d38e572979a0c8
{shahbaz-hafeez} <{shahbazhafeez888@gmail.com}>
2023-03-18 19:40:04 +0500

    first commit

---
+++ Random Password Generator/js/app.js
@@ -0,0 +1,61 @@
+const upperSet = "ABCEDFGHIJKLMNOPQRSTUVWXYZ";
+const lowerSet = "abcdefghijklmnopqrstuvwxyz";
+const numberSet = "1234567890";
+const symbolSet = "!@#$%^&*";
+
+
+const passBox = document .getElementById("pass-box")
+const totalChar = document.getElementById("total-char");
+const upperInput = document.getElementById("upper-case");
+const lowerInput = document.getElementById("lower-case");
+const numberInput = document.getElementById("numbers");
+const symbolInput = document.getElementById("symbols");
+
+
+const getRandomData = (dataset) => {
+
+  return dataset[Math.floor(Math.random() * dataset.length)]
+
+}
+
+const generatePassword = (password = "") =>{
+
+    if (upperInput.checked) {
+        password = password + getRandomData(upperSet);
+    }
+    if (lowerInput.checked) {
+        password = password + getRandomData(lowerSet);
+    }
+    if (numberInput.checked) {
+        password = password + getRandomData(numberSet);
+    }
+    if (symbolInput.checked) {
+        password = password + getRandomData(symbolSet);
+    }
+    if (password.length < totalChar.value) {
+         
+        return generatePassword(password);
+    }
+    
+     passBox.innerText = truncateString(password,totalChar.value);
+}
+
+generatePassword();
+
+document.getElementById("btn").addEventListener(
+    "click",
+    function(){
+
+        generatePassword();
+    }
+)
+
+function truncateString(str, num) {
+    if (str.length > num) {
+        let subStr = str.slice(0, num);
+        return subStr;
+    } else{
+        return str;
+    }
+}
+
